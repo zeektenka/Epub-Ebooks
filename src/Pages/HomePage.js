5a45c834eb1782ce530c354cb2390df906c5595a
zeektenka <zeektenka@gmail.com>
2022-01-19 19:33:30 +0200

    1 file modified

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,11 +5,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const links = DATA.map((item, index) => {
     return (
-      <li style={{ padding: '10px' }}>
-        {index + 1}{' '}
-        <Link key={item.id} to={item.path}>
-          {item.uri}
-        </Link>
+      <li key={item.id} style={{ padding: '10px' }}>
+        {index + 1} <Link to={item.path}>{item.uri}</Link>
       </li>
     );
   });
